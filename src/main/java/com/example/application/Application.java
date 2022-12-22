6f4e6b2c76754fee573b8f69e6560f7d7b191ea9
bawarzec <bartlomiejwarzecha2@gmail.com>
2022-12-22 17:10:08 +0100

    New build

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,21 @@
+package com.example.application;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.vaadin.artur.helpers.LaunchUtil;
+
+/**
+ * The entry point of the Spring Boot application.
+ */
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+    }
+
+}
